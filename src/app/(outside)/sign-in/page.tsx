4c065c7d6b71121c43880c477957b754a3bfdd62
Orlein <pcj9024@gmail.com>
2023-07-26 23:30:22 +0900

    fix login -> sign-in

---
+++ src/app/(outside)/sign-in/page.tsx
@@ -0,0 +1,103 @@
+'use client';
+import FullPageLoading from '@/common/components/misc/FullPageLoading';
+import useAuthSlice from '@/features/authSlice/useAuthSlice';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Link } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+export default function SignInPage() {
+  const {
+    signInState,
+    authState: { handleSignIn, loading, error },
+  } = useAuthSlice();
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> =
+    React.useCallback(
+      (event) => {
+        event.preventDefault();
+        handleSignIn();
+      },
+      [handleSignIn]
+    );
+  if (loading) {
+    return <FullPageLoading />;
+  }
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          onChange={signInState.handleChangeNewAuthSignInEmail}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          onChange={signInState.handleChangeNewAuthSignInPassword}
+        />
+
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+      {signInState.error && (
+        <Typography variant="caption" color="error">
+          {signInState.error}
+        </Typography>
+      )}
+      {error && (
+        <Typography variant="caption" color="error">
+          {error}
+        </Typography>
+      )}
+      <Link href="/sign-up" variant="body2">
+        {"Don't have an account? Sign Up"}
+      </Link>
+    </Box>
+  );
+}
