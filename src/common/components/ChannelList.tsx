10dbd808a28a3b5052bf4b4bdd80e7b6c61f6c5d
Orlein <pcj9024@gmail.com>
2023-07-27 22:11:05 +0900

    fix channel highlighting

--- src/common/components/ChannelList.tsx
+++ src/common/components/ChannelList.tsx
@@ -7,6 +7,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export type ChannelListProps = {
   open: boolean;
@@ -14,10 +15,12 @@ export type ChannelListProps = {
 
 export default function ChannelList(props: ChannelListProps) {
   useChannelListener();
+  const pathname = usePathname();
   const { channelState } = useChannelSlice();
 
   return (
     <List>
+      <ListItemText primary="Channels" />
       {channelState.channels.map((channel) => (
         <ListItem key={channel.id} disablePadding sx={{ display: 'block' }}>
           <ListItemButton
@@ -25,6 +28,10 @@ export default function ChannelList(props: ChannelListProps) {
               minHeight: 48,
               justifyContent: props.open ? 'initial' : 'center',
               px: 2.5,
+              backgroundColor:
+                pathname === `/c/${channel.id}`
+                  ? 'primary.main'
+                  : 'transparent',
             }}
             LinkComponent={Link}
             href={`/c/${channel.id}`}
